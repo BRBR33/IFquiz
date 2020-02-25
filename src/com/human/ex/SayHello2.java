85dde54643493c519d5901d14fa9121719e7da94
HU-06 <HU-06@192.168.0.95>
2020-02-25 15:15:50 +0900

    200225

---
+++ src/com/human/ex/SayHello2.java
@@ -0,0 +1,26 @@
+package com.human.ex;
+
+public class SayHello2 {
+
+	public static void main(String[] args) {
+
+	java.util.Scanner sc = new java.util.Scanner(System.in);
+		
+		System.out.print("안녕하세요 몇 번 말할래? : ");
+		String str = sc.nextLine();
+		int x = Integer.parseInt(str);
+		int a=0; //초기값
+		while(a<x) {//비교값
+			System.out.println("안녕하세요");
+			a++; //변환값
+			
+		}
+	
+		
+		for(int i=0;i<5;i++) {
+			System.out.println("안녕");
+		}
+		sc.close();
+	}
+
+}
